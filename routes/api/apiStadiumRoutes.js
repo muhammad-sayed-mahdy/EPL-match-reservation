346731a7678a84028d50315536ae2c9dc1839bd5
Omar <IamAdeadfish@hotmail.com>
2021-02-05 21:37:47 +0200

    [fixed] stadium routes doesn't need id

--- routes/api/apiStadiumRoutes.js
+++ routes/api/apiStadiumRoutes.js
@@ -4,7 +4,7 @@ const stadController = require("../../controllers/stadiumController");
 const router = Router();
 
 router.post('/', stadController.verifyStore(), stadController.store);
-router.put('/:id', stadController.verifyUpdate(), stadController.update);
-router.delete('/:id', stadController.verifyDestroy(), stadController.destroy);
+router.put('/', stadController.verifyUpdate(), stadController.update);
+router.delete('/', stadController.verifyDestroy(), stadController.destroy);
 
 module.exports = router;
