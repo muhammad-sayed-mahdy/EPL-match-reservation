429be3021b92a58d8e7e8ef108de9444ebe25d74
Evraa <evramyousef@gmail.com>
2021-04-06 19:46:23 +0200

    admin name

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -28,11 +28,7 @@ const getAllUsers = (req, res) => {
     try {
         User.find().sort({createdAt:-1}).then( (result) => {
             // res.status(200).json(result);
-            if(req.user == undefined)
-                user_name = "Evram";
-            else
-                user_name = req.user.name;
-            res.render("admin_profile", {match:result, title:"Profile", users:result, user_name:user_name});
+            res.render("admin_profile", {match:result, title:"Profile", users:result, user_name:req.user.name});
         });
     }
     catch (error) {
