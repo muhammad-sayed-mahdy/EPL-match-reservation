776583a430516cb837d866a852abb2c6de47996a
Omar <IamAdeadfish@hotmail.com>
2021-04-06 22:38:40 +0200

    [added] show reserved seats for user [forloop]

--- routes/api/apiReservationRoutes.js
+++ routes/api/apiReservationRoutes.js
@@ -6,6 +6,7 @@ const router = Router();
 
 router.post('/', requireAuth, reserveController.verifyReserve(), reserveController.reserve_post);
 router.put('/cancel', requireAuth, reserveController.verifyCancelReserve(), reserveController.cancelReserve_put);
-router.get('/show', requireAuth, reserveController.showReserve_get);
+router.get('/show', requireAuth, reserveController.showReserve_get);        // get reserved seat of a certain match
+router.get('/reserved', requireAuth, reserveController.getReservedseats);   // get reserved seats for a certain user for all matches
 
 module.exports = router;
