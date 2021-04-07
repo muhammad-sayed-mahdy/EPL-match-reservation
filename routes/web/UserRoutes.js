16cc09b99f93d5b8c5643b7119eb9d6325c93268
Muhammad Sayed <muhammad.mahmoud98@eng-st.cu.edu.eg>
2021-04-07 02:17:50 +0200

    fix bugs

--- routes/web/UserRoutes.js
+++ routes/web/UserRoutes.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 const userController = require("../../controllers/UserController");
-const { userRoute ,userInfo} = require('../../middleware/auth');
+const { userRoute } = require('../../middleware/auth');
 
 const router = Router();
 
 router.get('/update_profile', userRoute, userController.edit);
-router.get('/profile', userInfo, userController.view_profile);
+router.get('/profile', userRoute, userController.view_profile);
 router.get('/:id', userController.view);
 
 module.exports = router;
