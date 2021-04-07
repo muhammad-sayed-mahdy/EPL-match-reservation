53b7c48a481f41f102207f955190751581e44d91
Evraa <evramyousef@gmail.com>
2021-04-07 05:46:45 +0200

    waiting

--- routes/web/UserRoutes.js
+++ routes/web/UserRoutes.js
@@ -3,9 +3,10 @@ const userController = require("../../controllers/UserController");
 const { userRoute } = require('../../middleware/auth');
 
 const router = Router();
-
+// /user/
 router.get('/update_profile', userRoute, userController.edit);
 router.get('/profile', userRoute, userController.view_profile);
+router.get('/reservations', userRoute, userController.reservations);
 router.get('/:id', userController.view);
 
 module.exports = router;
